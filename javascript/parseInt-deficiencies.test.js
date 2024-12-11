1d1513a67194e267001149aff1cedcbbac7f1631
Michael D. Stemle, Jr. <mike.stemle@washpost.com>
2024-12-11 16:24:01 -0500

    Adding a couple more cases.

--- javascript/parseInt-deficiencies.test.js
+++ javascript/parseInt-deficiencies.test.js
@@ -7,8 +7,12 @@ describe('basic parseInt() use cases', () => {
     'one two three',
     '1,200,300',
     new Number(1234),
+    '2e5',
+    9007199254740995,
     '1 horse',
     'horse 123',
+    undefined,
+    null,
   ])('parseInt(%s)', (arg) => {
     console.debug(`${arg} -> «${parseInt(arg)}»`)
   })
